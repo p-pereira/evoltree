5199f9bd001ee873656d5906a7fc5e9488681b20
p-pereira <a64871@alunos.uminho.pt>
2020-11-29 22:14:05 +0000

    Update UI.py
    Interface updates (unfinished!)

--- src/UI.py
+++ src/UI.py
@@ -4,22 +4,29 @@ Created on Sat Nov 21 22:35:09 2020
 
 @author: pedro
 """
-from flask import Flask, render_template, Response, redirect
+from flask import Flask, render_template, Response, redirect, request
 from math import log
 import time
 import json
 import numpy as np
 from mgedt import mgedt_gui
 from threading import Thread
+import os
+import webbrowser
 
 UPDATE_RATE=1
+global PARAMS_SET
+PARAMS_SET=False
+global PARAMS_USER
+PARAMS_USER = []
+
 app = Flask(__name__, template_folder='web')
 thread = Thread(target = mgedt_gui)
 
 def update_interace_info():
     from stats.stats import stats
     from utilities.stats import trackers
-    from algorith.parameters import params
+    from algorithm.parameters import params
     generation = 0
     total_gens = params["GENERATIONS"]
     vid_dict = {}
@@ -72,8 +79,6 @@ def prog():
 
 @app.route('/start', methods=['POST', 'GET'])
 def start():
-    from algorithm.parameters import set_params
-    set_params('')
     thread.start()
     return redirect("prog")
 
@@ -81,14 +86,51 @@ def start():
 def progress():
     return Response(update_interace_info(), mimetype= 'text/event-stream')
 
-@app.route('/default_params')
-def send_default_params():
-    return Response(get_default_params(), mimetype= 'text/event-stream')
-    
-def get_default_params():
-    from algorithm.parameters import default_params
-    ret_string = "data:" + json.dumps(default_params) + "\n\n"
+@app.route('/parameters', methods=['POST', 'GET'])
+def parameters():
+    if request.method == 'POST':
+        args = parse_args(request.form)
+        from algorithm.parameters import set_params
+        set_params(args)
+        global PARAMS_SET
+        PARAMS_SET=True
+        global PARAMS_USER
+        PARAMS_USER=args
+        return redirect("/")
+    else:
+        return render_template('parameters.html')
+
+@app.route('/check_params')
+def check_params():
+    return Response(check_params_aux(), mimetype= 'text/event-stream')
+
+def check_params_aux():
+    global PARAMS_SET
+    ret_string = "data:" + json.dumps(PARAMS_SET) + "\n\n"
     yield ret_string
 
+def get_default_params():
+    from UI_parameters import schema
+    ret_string = "data:" + json.dumps(schema) + "\n\n"
+    yield ret_string
+
+def start_app():
+    if not os.environ.get("WERKZEUG_RUN_MAIN"):
+        webbrowser.open_new("http://127.0.0.1:5000/")
+    app.run(debug=False)
+
+def parse_args(args):
+    list_args = []
+    for key, val in args.items():
+        if val == "True":
+            list_args.append("--"+key)
+        elif val == "False" or val=="":
+            continue
+        else:
+            list_args.append("--"+key+"="+val)
+    return list_args
+
+
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    start_app()
