3b51541f2109d9e39965f9b066c570af8f4816a9
p-pereira <a64871@alunos.uminho.pt>
2020-11-29 22:15:21 +0000

    Update mgedt.py
    Class created, better organization for package creation

--- src/mgedt.py
+++ src/mgedt.py
@@ -11,30 +11,130 @@ from os import path
 
 check_python_version()
 
-from stats.stats import get_stats
-from algorithm.parameters import params, set_params
-import sys
-
 from warnings import simplefilter
 # ignore all future warnings
 simplefilter(action='ignore', category=FutureWarning)
 
+class MGEDT(object):
+    """
+    MGEDT object.
+    """
+    def __init__(self, pop=10, gen=100, lamarck=True, multicore=True, 
+                 **extra_params):
+        from stats.stats import stats
+        from algorithm.parameters import params, set_params
+        from multiprocessing import Pool
+        from utilities.algorithm.initialise_run import pool_init
+        from operators.initialisation import initialisation
+        from fitness.evaluation import evaluate_fitness
+        
+        param_list = ['--population_size={0}'.format(str(pop)),
+                      '--generations={0}'.format(str(gen))]
+        if multicore:
+            param_list.append("--multicore")
+        if lamarck:
+            param_list.append("--lamarck")
+        
+        for (key, val) in extra_params.items():
+            if val == "True":
+                param_list.append("--"+key)
+            elif val=="False" or val=="":
+                continue
+            elif key == "X_train":
+                params[key] = val
+            elif key == "y_train":
+                params[key] = val
+            elif key == "X_test":
+                params[key] = val
+            elif key == "y_test":
+                params[key] = val
+            else:
+                param_list.append("--{0}={1}".format(key, val))
+        set_params(param_list)
+        
+        if multicore:
+            if "POOL" in params.keys():
+                params["POOL"] = None
+            # initialize pool once, if mutlicore is enabled
+            params['POOL'] = Pool(processes=params['CORES'], 
+                                  initializer=pool_init,
+                                  initargs=(params,))
+        # Initialise population
+        individuals = initialisation(pop)
+        # Evaluate initial population
+        individuals = evaluate_fitness(individuals)
+        self.params = params
+        self.population = individuals
+        self.stats = stats
+    
+    def evolve(self):
+        from stats.stats import get_stats, stats
+        from algorithm.parameters import params
+        from tqdm import tqdm
+        
+        population = self.population
+        # Generate statistics for run so far
+        get_stats(population)
+        
+        total_gens = params['GENERATIONS']+1
+        # Traditional GE
+        for generation in tqdm(range(1, total_gens)):
+            stats['gen'] = generation
+            population = params['STEP'](population)
+        
+        get_stats(population, end=True)
+        self.stats = stats
+        self.population = population
+    
+    def reevolve(self, generations):
+        from stats.stats import get_stats, stats
+        from algorithm.parameters import params
+        from tqdm import tqdm
+        
+        population = self.population
+        # Generate statistics for run so far
+        get_stats(population)
+        
+        total_gens = params['GENERATIONS']+1 + generations
+        # Traditional GE
+        for generation in tqdm(range(params['GENERATIONS']+1, total_gens)):
+            stats['gen'] = generation
+            population = params['STEP'](population)
+        
+        get_stats(population, end=True)
+        self.stats = stats
+        self.population = population
+    
+
 def set_params1(train_data, test_data, target, # data parameters are mandatory
-          pop=12, gen=100, sampling=1000, 
-          lamarck=True, sep=";", multicore=True):
+          delimiter=";", pop=12, gen=100, sampling=1000, 
+          lamarck=True, multicore=True, **extra_params):
     param_list = ['--population_size={0}'.format(str(pop)),
                   '--generations={0}'.format(str(gen)),
                   '--dataset_train={0}'.format(train_data),
                   '--dataset_test={0}'.format(test_data),
-                  '--dataset_delimiter={0}'.fotmat(sep),
-                  '--target={0}'.format(target),
-                  '--multicore={0}'.format(str(multicore))]
+                  '--dataset_delimiter={0}'.format(delimiter),
+                  '--target={0}'.format(target)]
+    if multicore:
+        param_list.append("--multicore")
+    if lamarck:
+        param_list.append("--lamarck")
+    for (key, val) in extra_params.items():
+        if val == "True":
+            param_list.append("--"+key)
+        elif val=="False" or val=="":
+            continue
+        else:
+            param_list.append("--{0}={1}".format(key, val))
+    #set_params(param_list)
     
-    set_params(param_list)
+    return param_list
 
 def evolve():
     #import GE
     """ Run program """
+    from stats.stats import get_stats
+    from algorithm.parameters import params
     
     # Run evolution
     individuals = params['SEARCH_LOOP']()
@@ -49,6 +149,7 @@ def evolve():
     return individuals, results
 
 def mgedt(parametrization="auto"):
+    from algorithm.parameters import set_params
     if parametrization == "auto":
         set_params('')
     pop, res = evolve()
@@ -93,7 +194,7 @@ def mgedt_gui():
     get_stats(individuals, end=True)
     
 
-if __name__ == "__main__":
-    set_params(sys.argv[1:])
-    _ = evolve()
-    #mane()
+#if __name__ == "__main__":
+#    #set_params(sys.argv[1:])
+#    #_ = evolve()
+#    #mane()
