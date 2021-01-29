744b7deb674f2da344b6f64774fcc0ab0a0b0023
p-pereira <a64871@alunos.uminho.pt>
2021-01-29 22:04:00 +0000

    Update runner.py

--- src/runner.py
+++ src/runner.py
@@ -9,15 +9,10 @@ from mgedt import MGEDT
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import roc_auc_score, roc_curve
-from sklearn.metrics import plot_roc_curve
+#from sklearn.metrics import plot_roc_curve
 import matplotlib.pyplot as plt
 
-mgedt = MGEDT()
-mgedt.params
-mgedt.param_list
-
 d = pd.read_csv("../datasets/Promos/TEST2/Train-IDF-1.csv", sep=";")
-d.columns
 
 dtrain, dtest = train_test_split(d, test_size=0.3, stratify=d['target'])
 dtrain, dval = train_test_split(dtrain, test_size=0.3, stratify=dtrain['target'])
@@ -27,16 +22,13 @@ X_val = dval.drop('target', axis=1)
 y_val = dval['target']
 X_test = dtest.drop('target', axis=1)
 y_test = dtest['target']
+mgedt = MGEDT(pop=20)#X_train=X, y_train=y, X_test=X_val, y_test=y_val)
 mgedt.fit(X, y, X_val, y_val)
-mgedt.params
 mgedt.predict(X_test)
 pred = mgedt.predict(X_test)
 
-roc_auc_score(y_test, pred)
-
 ROC = pd.DataFrame(roc_curve(y_test, pred, pos_label='NoSale')).T
 ROC.columns = ["FPR", "TPR", "TH"]
-ROC
 
 plt.plot(ROC['FPR'], ROC['TPR'], color='darkorange')
 plt.show()
